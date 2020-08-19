2f97a0ba8cd50ed9c5d0e5f3c797fc88ddcdffea
ctdupuis <ctdupuis7@gmail.com>
2020-08-18 21:38:14 -0500

    added icons and basic styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('loaded')
+    console.log('loaded');
+    let pTag = document.querySelector('#header');
+
+    pTag.addEventListener('click', (event) => toggleColor(event))
+
+    function toggleColor(event) {
+        debugger
+    //    if (event.target.className === "test") {
+    //        event.target.className = 'highlited'
+    //    } else {
+    //        event.target.className = 'test'
+    //    }
+    //    while (event.target.out)
+    //    debugger
+    }
+
+    // pTag.addEventListener("mouseover", function( event ) {   
+    //     // highlight the mouseover target
+    //     event.target.style.color = "orange";
+      
+    //     // reset the color after a short delay
+    //     setTimeout(function() {
+    //       event.target.style.color = "";
+    //     }, 500);
+    //   }, false);
+    
+
 })
